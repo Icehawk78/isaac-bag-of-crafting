3a1421c17b0c407c71fad2dd3d349af52b6d66e4
Nicholas Galauxy <ngalauxy@civisanalytics.com>
2021-10-01 13:45:55 -0500

    Unverified changes

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,14 @@ import { Combinations } from './Combinations';
 
 const _ = require('lodash');
 
+const items = XmlParser.loadItems(fs.readFileSync('assets/items.xml', 'utf8'));
 const pools = XmlParser.loadPools(fs.readFileSync('assets/itempools.xml', 'utf8'));
 const meta = XmlParser.loadMeta(fs.readFileSync('assets/items_metadata.xml', 'utf8'));
 const bc = new BagOfCrafting(pools, meta);
+
+console.log('Pools: ', pools[0]['items'][0]);
+console.log('Meta: ', meta);
+console.log(bc.calculate([1,1,1,1,8,8,8,8]));
 
 interface Hash<T> {
   [key: string]: T;
@@ -46,10 +51,40 @@ const craftParts: Hash<CraftItem> = {
   'y': {'name': 'Dice Shard', 'id': 24},
   'z': {'name': 'Key Shard', 'id': 25},
 }
+
+let inputs = 'bccpppiiim'
 // 'bbbbbbbb' to [1,1,1,1,1,1,1,1]
-const asciiToNum = (s: string) => s.split('').map(c => craftParts[c]['id']).sort();
+const asciiToNum = (s: string) => s.split('').map(c => craftParts[c]['id']);
 
-let inputs = 'bccijmpppqw';
-let c: number[][] = _.uniq(Combinations(asciiToNum(inputs), 8));
+const prettyCount = (x: string[]) => {
+  const counts = _.countBy(x, (y: string) => craftParts[y].name);
+  return Object.keys(counts).map(k => `${k}*${counts[k]}`).join(', ')
+}
 
-c.forEach(i => console.log(i, bc.calculate(i)));
+const prettyResults = (results: Result[]) => {
+  const grouped = _.groupBy(results, 'item');
+  return Object.keys(grouped).map(k => [k, _.uniq(grouped[k].map((v: Result) => v.recipe))]);
+}
+
+let c: string[][] = _.uniq(Combinations(inputs.split(''), 8).map(x => x.sort()));
+
+interface Result {
+  recipe: string,
+  item: string,
+}
+
+const results: Result[] = c.map(i => {
+  const item = items.get(bc.calculate(i.map(x => craftParts[x]['id'])));
+  const result: Result = {
+    recipe: prettyCount(i),
+    item: '???',
+  };
+  if (item) {
+    result.item = item['name'];
+  }
+  return result;
+});
+
+console.log(prettyResults(results));
+
+// c.forEach(i => console.log(i.join(), bc.calculate(i)));
